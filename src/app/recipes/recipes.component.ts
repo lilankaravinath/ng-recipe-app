f6f5a54371bf1dd1e4cb2d71e036a75244522472
Lilanka Ravinath <inbox.lilanka@gmail.com>
2020-05-10 13:23:44 +0530

    Use routing

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -11,7 +11,7 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
-  
+
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
@@ -21,7 +21,6 @@ export class RecipesComponent implements OnInit {
             this.selectedRecipe = recipe;
           }
         );
-      
   }
 
 }
