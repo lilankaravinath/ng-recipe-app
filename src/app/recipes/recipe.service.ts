81eb4f01a4e9944895185efbd9609816d4e3c34b
Lilanka Ravinath <inbox.lilanka@gmail.com>
2020-05-14 15:52:30 +0530

    Use HttpRequest (Backend Firebase)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,53 +9,61 @@ export class RecipeService {
 
   recipeChanged = new Subject<Recipe[]>();
 
-   private recipes: Recipe[] = [
-      new Recipe(
-        'Tasty Schnitzel',
-        'A super-tasty Schnitzel - just awesome!',
-        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-        [
-          new Ingredient('Meat', 1),
-          new Ingredient('French Frice', 20)
-        ]
-        ),
-      new Recipe(
-        'Big Fat Burger',
-        'What else you need to say?',
-        'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-        [
-          new Ingredient('Buns', 1),
-          new Ingredient('Meat', 1)
-        ]
-        )
-    ];
+  //  private recipes: Recipe[] = [
+  //     new Recipe(
+  //       'Tasty Schnitzel',
+  //       'A super-tasty Schnitzel - just awesome!',
+  //       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+  //       [
+  //         new Ingredient('Meat', 1),
+  //         new Ingredient('French Frice', 20)
+  //       ]
+  //       ),
+  //     new Recipe(
+  //       'Big Fat Burger',
+  //       'What else you need to say?',
+  //       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+  //       [
+  //         new Ingredient('Buns', 1),
+  //         new Ingredient('Meat', 1)
+  //       ]
+  //       )
+  //   ];
 
-    constructor(private shoppingListService: ShoppingListService) {}
+  private recipes: Recipe[] = [];
 
-    getRecipes() {
-       return this.recipes.slice();
-    }
+  constructor(private shoppingListService: ShoppingListService) {}
 
-    getRecipe(index: number) {
-      return this.recipes[index];
-    }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]) {
-      this.shoppingListService.addIngredients(ingredients);
-    }
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 
-    addRecipe(recipe: Recipe) {
-      this.recipes.push(recipe);
-      this.recipeChanged.next(this.recipes.slice());
-    }
+  getRecipes() {
+      return this.recipes.slice();
+  }
 
-    updateRecipe(index: number, newRecipe: Recipe) {
-      this.recipes[index] = newRecipe;
-      this.recipeChanged.next(this.recipes.slice());
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    deleteRecipe(index: number) {
-      this.recipes.splice(index, 1);
-      this.recipeChanged.next(this.recipes.slice());
-    }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipeChanged.next(this.recipes.slice());
+  }
 }
