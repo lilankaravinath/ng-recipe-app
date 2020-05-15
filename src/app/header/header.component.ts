62bb3bca274cda22162ef3e7b25ccc50012d8e5f
Lilanka Ravinath <inbox.lilanka@gmail.com>
2020-05-15 11:36:12 +0530

    Add Authentication and Route Protection

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   collapsed = true;
-  constructor( private dataStorageService: DataStorageService) { }
+  isAuthenticated = false;
+  private userSubscription: Subscription;
+
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.userSubscription = this.authService.user
+      .subscribe(user => {
+        this.isAuthenticated = !!user; // !user ? false : true;
+      });
   }
 
   onSaveData(){
@@ -18,8 +32,15 @@ export class HeaderComponent implements OnInit {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes()
-      .subscribe();
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy(){
+    this.userSubscription.unsubscribe();
   }
 
 
